9b1caef45ab2c3f07913d21426227bbfc5d5f635
weslley543 <weslleydrum@hotmail.com>
2020-05-23 18:58:08 -0300

    plans functions done

--- src/app/controllers/PlansController.js
+++ src/app/controllers/PlansController.js
@@ -33,6 +33,29 @@ class PlansContoller {
     }
     return res.status(400).json({ message: 'Plan does not exist' });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      price: Yup.number(),
+      duration: Yup.number(),
+      id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ msg: 'Id not received' });
+    }
+
+    const plan = await Plans.findByPk(req.body.id);
+
+    if (!plan) {
+      return res.status(401).json({ msg: 'Plan does not exists !' });
+    }
+
+    const { title, price, duration } = await plan.update(req.body);
+
+    return res.status(200).json({ title, price, duration });
+  }
 }
 
 export default new PlansContoller();
